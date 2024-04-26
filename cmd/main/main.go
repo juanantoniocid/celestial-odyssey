514264c85de60aa21fec23f251042e0d492a361a
Juan Antonio Cid Tomas <juanantonio.cid@cabify.com>
2024-04-26 16:26:39 +0200

    game: Move game initialization logic to game package

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -3,37 +3,28 @@ package main
 import (
 	"log"
 
-	"github.com/hajimehoshi/ebiten/v2"
-	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
-
 	"celestial-odyssey/internal/game"
-	"celestial-odyssey/world/entities"
+	"github.com/hajimehoshi/ebiten/v2"
+)
+
+const (
+	windowWidth  = 320
+	windowHeight = 200
+	scale        = 3.0
 )
 
 func main() {
-	playerImage := loadPlayerImage()
-	player := entities.NewPlayer(100, 100, 0, playerImage)
+	setWindow()
 
-	gameInstance := game.NewGame(player)
-
-	screenWidth, screenHeight := 320, 200
-	scale := 3.0
-
-	ebiten.SetWindowTitle("Celestial Odyssey")
-	ebiten.SetWindowSize(int(float64(screenWidth)*scale), int(float64(screenHeight)*scale))
-	ebiten.SetWindowResizingMode(ebiten.WindowResizingModeEnabled)
-	ebiten.SetScreenClearedEveryFrame(true)
-
+	gameInstance := game.NewGame(windowWidth, windowHeight)
 	if err := ebiten.RunGame(gameInstance); err != nil {
 		log.Fatal(err)
 	}
 }
 
-func loadPlayerImage() *ebiten.Image {
-	img, _, err := ebitenutil.NewImageFromFile("assets/images/player.png")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	return img
+func setWindow() {
+	ebiten.SetWindowTitle("Celestial Odyssey")
+	ebiten.SetWindowSize(int(float64(windowWidth)*scale), int(float64(windowHeight)*scale))
+	ebiten.SetWindowResizingMode(ebiten.WindowResizingModeEnabled)
+	ebiten.SetScreenClearedEveryFrame(true)
 }
--- internal/game/game.go
+++ internal/game/game.go
@@ -1,34 +1,53 @@
 package game
 
 import (
+	"log"
+
 	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
 
 	"celestial-odyssey/internal/screen"
 	"celestial-odyssey/world/entities"
 )
 
 type Game struct {
-	ScreenManager *screen.Manager
+	screenManager *screen.Manager
+	width         int
+	height        int
 }
 
-func NewGame(player *entities.Player) *Game {
-	screenManager := screen.NewScreenManager(320, 200, player)
+func NewGame(gameWidth, gameHeight int) *Game {
+	playerImage := loadPlayerImage()
+	player := entities.NewPlayer(100, 100, 0, playerImage)
+
+	screenManager := screen.NewScreenManager(gameWidth, gameHeight, player)
 
 	return &Game{
-		ScreenManager: screenManager,
+		screenManager: screenManager,
+		width:         gameWidth,
+		height:        gameHeight,
 	}
 }
 
+func loadPlayerImage() *ebiten.Image {
+	img, _, err := ebitenutil.NewImageFromFile("assets/images/player.png")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return img
+}
+
 func (g *Game) Update() error {
-	g.ScreenManager.Update()
+	g.screenManager.Update()
 
 	return nil
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
-	g.ScreenManager.Draw(screen)
+	g.screenManager.Draw(screen)
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
-	return 320, 200 // Return the fixed game screen size
+	return g.width, g.height
 }
