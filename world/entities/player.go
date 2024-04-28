f2af6068415193b054d07f6a702cf822e2f09ce1
Juan Antonio Cid Tomas <juanantonio.cid@cabify.com>
2024-04-28 11:53:00 +0200

    entities: Reorder player functions

--- world/entities/player.go
+++ world/entities/player.go
@@ -25,22 +25,6 @@ func NewPlayer(img *ebiten.Image, playArea image.Rectangle) *Player {
 	}
 }
 
-func (p *Player) top() int {
-	return p.position.Y
-}
-
-func (p *Player) bottom() int {
-	return p.position.Y + p.hitbox.Dy()
-}
-
-func (p *Player) left() int {
-	return p.position.X
-}
-
-func (p *Player) right() int {
-	return p.position.X + p.hitbox.Dx()
-}
-
 func (p *Player) MoveLeft() {
 	p.moveLeft = true
 	p.position.X -= p.speed
@@ -49,22 +33,6 @@ func (p *Player) MoveLeft() {
 func (p *Player) MoveRight() {
 	p.moveRight = true
 	p.position.X += p.speed
-}
-
-func (p *Player) setPositionToLeftBoundary(origin int) {
-	p.position.X = origin
-}
-
-func (p *Player) MoveToRightBoundary(width int) {
-	p.position.X = width - p.hitbox.Dx()
-}
-
-func (p *Player) setPositionToTopBoundary(origin int) {
-	p.position.Y = origin
-}
-
-func (p *Player) MoveToBottomBoundary(height int) {
-	p.position.Y = height - p.hitbox.Dy()
 }
 
 func (p *Player) SetSpeed(speed int) {
@@ -85,6 +53,46 @@ func (p *Player) Update() {
 	p.enforceBoundaries()
 }
 
+func (p *Player) Draw(screen *ebiten.Image) {
+	op := &ebiten.DrawImageOptions{}
+	op.Filter = ebiten.FilterNearest
+	op.GeoM.Translate(float64(p.position.X), float64(p.position.Y))
+
+	screen.DrawImage(p.image, op)
+}
+
+func (p *Player) MoveToRightBoundary(width int) {
+	p.position.X = width - p.hitbox.Dx()
+}
+
+func (p *Player) MoveToBottomBoundary(height int) {
+	p.position.Y = height - p.hitbox.Dy()
+}
+
+func (p *Player) top() int {
+	return p.position.Y
+}
+
+func (p *Player) bottom() int {
+	return p.position.Y + p.hitbox.Dy()
+}
+
+func (p *Player) left() int {
+	return p.position.X
+}
+
+func (p *Player) right() int {
+	return p.position.X + p.hitbox.Dx()
+}
+
+func (p *Player) setPositionToLeftBoundary(origin int) {
+	p.position.X = origin
+}
+
+func (p *Player) setPositionToTopBoundary(origin int) {
+	p.position.Y = origin
+}
+
 func (p *Player) enforceBoundaries() {
 	if p.left() < p.playArea.Min.X {
 		p.setPositionToLeftBoundary(p.playArea.Min.X)
@@ -97,12 +105,4 @@ func (p *Player) enforceBoundaries() {
 	} else if p.bottom() > p.playArea.Max.Y {
 		p.MoveToBottomBoundary(p.playArea.Max.Y)
 	}
-}
-
-func (p *Player) Draw(screen *ebiten.Image) {
-	op := &ebiten.DrawImageOptions{}
-	op.Filter = ebiten.FilterNearest
-	op.GeoM.Translate(float64(p.position.X), float64(p.position.Y))
-
-	screen.DrawImage(p.image, op)
 }
