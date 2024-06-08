1bd91a8e6e547ba36509c29123bc7a1a6049574d
Juan Antonio Cid Tomas <juanantonio.cid@cabify.com>
2024-06-08 16:47:52 +0200

    config: Move setup to a different file

--- internal/config/config.go
+++ internal/config/config.go
@@ -8,11 +8,6 @@ import (
 	"celestial-odyssey/internal/util"
 )
 
-const (
-	screenWidth  = 320
-	screenHeight = 200
-)
-
 type Config struct {
 	Window Window
 	Screen Screen
@@ -47,42 +42,9 @@ type Player struct {
 
 func LoadConfig() Config {
 	return Config{
-		Window: loadWindow(),
-		Screen: loadScreen(),
-		Ground: loadGround(),
-		Player: loadPlayer(),
-	}
-}
-
-func loadWindow() Window {
-	return Window{
-		Title:        "Celestial Odyssey",
-		Dimensions:   util.Dimensions{Width: 960, Height: 600},
-		ResizingMode: ebiten.WindowResizingModeEnabled,
-	}
-}
-
-func loadScreen() Screen {
-	return Screen{
-		ClearedEveryFrame: true,
-		Dimensions:        util.Dimensions{Width: screenWidth, Height: screenHeight},
-		BackgroundColor:   color.RGBA{R: 24, G: 8, B: 50, A: 1},
-	}
-}
-
-func loadGround() Ground {
-	return Ground{
-		Dimensions: util.Dimensions{Width: 40, Height: 28},
-		File:       "assets/images/ground.png",
-	}
-}
-
-func loadPlayer() Player {
-	return Player{
-		Dimensions:          util.Dimensions{Width: 16, Height: 32},
-		WalkingVelocity:     2,
-		InitialJumpVelocity: -10,
-		Gravity:             0.5,
-		SpritesFile:         "assets/images/player.png",
+		Window: setupWindow(),
+		Screen: setupScreen(),
+		Ground: setupGround(),
+		Player: setupPlayer(),
 	}
 }
