39a11d3c9330a6e9ffd29cd10d5b7674ffc09be5
Juan Antonio Cid Tomas <juanantonio.cid@cabify.com>
2024-07-30 10:01:00 +0200

    game: Remove types file.

--- internal/game/game.go
+++ internal/game/game.go
@@ -4,11 +4,13 @@ import (
 	"github.com/hajimehoshi/ebiten/v2"
 
 	"celestial-odyssey/internal/config"
+	"celestial-odyssey/internal/entity"
 	"celestial-odyssey/internal/system/behavior"
 	"celestial-odyssey/internal/system/graphics"
 	"celestial-odyssey/internal/util"
 )
 
+// Game represents the game itself.
 type Game struct {
 	levels       []Level
 	currentLevel int
@@ -17,6 +19,27 @@ type Game struct {
 	renderer     graphics.Renderer
 
 	dimensions util.Dimensions
+}
+
+// Level represents a game level, which can contain multiple sections.
+type Level interface {
+	// Init initializes the level with the given parameters.
+	Init()
+
+	// AddSection adds a section to the level.
+	AddSection(section Section)
+
+	// CurrentSection returns the current section.
+	CurrentSection() Section
+}
+
+// Section represents a single scenario within a level.
+type Section interface {
+	// Entities returns the entities in the scenario.
+	Entities() *entity.Entities
+
+	// SetPlayerPositionAtLeft sets the player position on the left side of the screen.
+	SetPlayerPositionAtLeft()
 }
 
 func NewGame(cfg config.Screen, updateSystem behavior.UpdateSystem, renderer graphics.Renderer) *Game {
@@ -40,20 +63,18 @@ func (g *Game) AddLevel(level Level) {
 }
 
 func (g *Game) Update() error {
-	currentLevel := g.levels[g.currentLevel]
-	currentSection := currentLevel.CurrentSection()
-	entities := currentSection.Entities()
-
-	g.updateSystem.Update(entities)
+	g.updateSystem.Update(g.sectionEntities())
 	return nil
 }
 
-func (g *Game) Draw(screen *ebiten.Image) {
+func (g *Game) sectionEntities() *entity.Entities {
 	currentLevel := g.levels[g.currentLevel]
 	currentSection := currentLevel.CurrentSection()
-	entities := currentSection.Entities()
+	return currentSection.Entities()
+}
 
-	g.renderer.Draw(screen, entities)
+func (g *Game) Draw(screen *ebiten.Image) {
+	g.renderer.Draw(screen, g.sectionEntities())
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
